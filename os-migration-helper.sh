0622f411a6b11dea5632031e8751ac3da8e09edf
Othman Madjoudj <omadjoudj@mirantis.com>
2022-11-30 14:50:43 +0100

    Add verify confirmation for VMs in shutdown state

--- os-migration-helper.sh
+++ os-migration-helper.sh
@@ -4,9 +4,30 @@
 
 host=$1
 
+usage() {
+    echo "Usage: $0 <HOSTNAME>"
+    exit 1
+}
+
+[[ $# -eq 0 ]] && usage
+
 # Shutoff VMs
-echo "[WARN] Cold-migrating shutoff VMs from $host"
-for i in `openstack server list --all --host $host --status shutoff -f value -c ID` ; do openstack server migrate $i ; done
+#for i in `openstack server list --all --host $host --status shutoff -f value -c ID` ; do openstack server migrate $i ; done
+vms_in_shutoff_state=`openstack server list --all --host $host --status shutoff -f value -c ID`
+
+for i in $vms_in_shutoff_state; do
+    echo "[INFO] Cold-migrating shutoff VM: $i from $host"
+    openstack server migrate $i
+done
+sleep 10
+
+
+for i in $vms_in_shutoff_state; do
+    if [[ "$(openstack server show $i -f value -c status)" == "VERIFY_RESIZE" ]];
+        echo "[INFO] Confirming RESIZE for the VM: $i from $host"
+        openstack server migrate confirm $i
+    fi
+done
 
 while true; do
         total_vms=`openstack server list --all --host $host -f value -c ID | wc -l`
@@ -17,10 +38,10 @@ while true; do
 
         # Live evacuate
         vm_migrating=`openstack server list --all --host $host --status migrating -f value -c ID | wc -l`
-        echo "[INFO] Currently migrating $vm_migrating VM(s) from $host"
+        echo "[INFO] Currently $vm_migrating VM(s) are in MIGRATING state from $host"
         sleep 10
-        if [ $vm_migrating -eq 0 ] ; then
-                echo "[INFO] Live-Evacuating 20 VMs from $host"
+        if [[ $vm_migrating -eq 0 ]] ; then
+                echo "[INFO] Launching Live-Evacuation of 20 VMs from $host"
                 nova host-evacuate-live --max-servers 20 $host
         fi
 done
