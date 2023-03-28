2e06f489d3f6ab01da7ff98c61c45d7ff1762c04
Othman Madjoudj <omadjoudj@mirantis.com>
2023-03-28 14:14:36 +0200

    BM log collection: make the namespace an arg

--- mosk-log-collect-snippets/get-bm-logs.sh
+++ mosk-log-collect-snippets/get-bm-logs.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+NAMESPACE=$1
 
 ## Logs:
 
@@ -9,17 +10,17 @@ kubectl logs -n kaas --all-containers --timestamps deployment/kaas-ipam > kaas-i
 
 ## Objects:
 
-kubectl get -n <NAMESPACE> -o yaml BareMetalHostCredential > BareMetalHostCredential.all.yaml
-kubectl get -n <NAMESPACE> -o yaml BareMetalHost > BareMetalHost.all.yaml
-kubectl get -n <NAMESPACE> -o yaml BareMetalHostProfile > BareMetalHostProfile.all.yaml
-kubectl get -n <NAMESPACE> -o yaml L2Template > L2Template.all.yaml
-kubectl get -n <NAMESPACE> -o yaml LCMMachine > LCMMachine.all.yaml
-kubectl get -n <NAMESPACE> -o yaml IPAMHost > IPAMHost.all.yaml
+kubectl get -n $NAMESPACE -o yaml BareMetalHostCredential > BareMetalHostCredential.all.yaml
+kubectl get -n $NAMESPACE -o yaml BareMetalHost > BareMetalHost.all.yaml
+kubectl get -n $NAMESPACE -o yaml BareMetalHostProfile > BareMetalHostProfile.all.yaml
+kubectl get -n $NAMESPACE -o yaml L2Template > L2Template.all.yaml
+kubectl get -n $NAMESPACE -o yaml LCMMachine > LCMMachine.all.yaml
+kubectl get -n $NAMESPACE -o yaml IPAMHost > IPAMHost.all.yaml
 
 
-kubectl get -n <NAMESPACE> -o wide BareMetalHostCredential > BareMetalHostCredential.all.wide.txt
-kubectl get -n <NAMESPACE> -o wide BareMetalHost > BareMetalHost.all.wide.txt
-kubectl get -n <NAMESPACE> -o wide BareMetalHostProfile > BareMetalHostProfile.all.wide.txt
-kubectl get -n <NAMESPACE> -o wide L2Template > L2Template.all.wide.txt
-kubectl get -n <NAMESPACE> -o wide LCMMachine > LCMMachine.all.wide.txt
-kubectl get -n <NAMESPACE> -o wide IPAMHost > IPAMHost.all.wide.txt
+kubectl get -n $NAMESPACE -o wide BareMetalHostCredential > BareMetalHostCredential.all.wide.txt
+kubectl get -n $NAMESPACE -o wide BareMetalHost > BareMetalHost.all.wide.txt
+kubectl get -n $NAMESPACE -o wide BareMetalHostProfile > BareMetalHostProfile.all.wide.txt
+kubectl get -n $NAMESPACE -o wide L2Template > L2Template.all.wide.txt
+kubectl get -n $NAMESPACE -o wide LCMMachine > LCMMachine.all.wide.txt
+kubectl get -n $NAMESPACE -o wide IPAMHost > IPAMHost.all.wide.txt
