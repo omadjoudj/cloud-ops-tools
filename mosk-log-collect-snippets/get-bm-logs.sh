d501f21ec010596360ad78341122bbbbde00f23a
Othman Madjoudj <omadjoudj@mirantis.com>
2023-03-28 14:13:37 +0200

    Add some snippet for log collection on MOSK

---
+++ mosk-log-collect-snippets/get-bm-logs.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+
+## Logs:
+
+kubectl logs -n kaas --all-containers --timestamps deployment/baremetal-provider > baremetal-provider.log
+kubectl logs -n kaas --all-containers --timestamps deployment/baremetal-operator > baremetal-operator.log
+kubectl logs -n kaas --all-containers --timestamps deployment/kaas-ipam > kaas-ipam.log
+
+## Objects:
+
+kubectl get -n <NAMESPACE> -o yaml BareMetalHostCredential > BareMetalHostCredential.all.yaml
+kubectl get -n <NAMESPACE> -o yaml BareMetalHost > BareMetalHost.all.yaml
+kubectl get -n <NAMESPACE> -o yaml BareMetalHostProfile > BareMetalHostProfile.all.yaml
+kubectl get -n <NAMESPACE> -o yaml L2Template > L2Template.all.yaml
+kubectl get -n <NAMESPACE> -o yaml LCMMachine > LCMMachine.all.yaml
+kubectl get -n <NAMESPACE> -o yaml IPAMHost > IPAMHost.all.yaml
+
+
+kubectl get -n <NAMESPACE> -o wide BareMetalHostCredential > BareMetalHostCredential.all.wide.txt
+kubectl get -n <NAMESPACE> -o wide BareMetalHost > BareMetalHost.all.wide.txt
+kubectl get -n <NAMESPACE> -o wide BareMetalHostProfile > BareMetalHostProfile.all.wide.txt
+kubectl get -n <NAMESPACE> -o wide L2Template > L2Template.all.wide.txt
+kubectl get -n <NAMESPACE> -o wide LCMMachine > LCMMachine.all.wide.txt
+kubectl get -n <NAMESPACE> -o wide IPAMHost > IPAMHost.all.wide.txt
