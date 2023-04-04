3278fd0b254173e94b2abcd7800eb54205357b74
Othman Madjoudj <omadjoudj@mirantis.com>
2023-04-04 19:12:40 +0200

    Uncomment temp. kubectl autocomplete

--- kubectl-alias.sh
+++ kubectl-alias.sh
@@ -2,7 +2,7 @@ alias k=kubectl
 complete -o default -F __start_kubectl k
 
 # set up autocomplete in bash into the current shell, bash-completion package should be installed first.
-# source <(kubectl completion bash)
+source <(kubectl completion bash)
 
 # add autocomplete permanently to your bash shell.
 echo "source <(kubectl completion bash)" >> ~/.bashrc 
