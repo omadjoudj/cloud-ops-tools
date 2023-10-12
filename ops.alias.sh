af29ed9edad09324fa8f515b0931d9974c61d605
Othman Madjoudj <omadjoudj@mirantis.com>
2023-10-12 12:59:44 +0200

    Add some usuful aliases for working with MCC/MOSK

--- ops.alias.sh
+++ ops.alias.sh
@@ -1,4 +1,12 @@
+alias k='kubectl'
+#source <(kubectl completion bash)
+. ~/.kubectl_bash_compl
+complete -o default -F __start_kubectl k
+
+alias mcc="export KUBECONFIG=$PWD/mcc.kubeconfig"
+alias mosk="export KUBECONFIG=$PWD/mosk.kubeconfig"
+alias rook="kubectl exec -n rook-ceph deploy/rook-ceph-tools -it --"
+alias os="kubectl exec -it -n openstack deploy/keystone-client -it --"
 alias silences="kubectl -n stacklight exec sts/prometheus-alertmanager -c prometheus-alertmanager -- amtool --alertmanager.url http://127.0.0.1:9093 silence"
 alias silence-add="kubectl -n stacklight exec sts/prometheus-alertmanager -c prometheus-alertmanager -- amtool --alertmanager.url http://127.0.0.1:9093 silence add -a '<YOUR_NAME>' -d '2h'"
-
-silence-add -c "Comment" alertname=~".+"
+alias silence-all='silence-add -c "Comment" alertname=~".+"'
