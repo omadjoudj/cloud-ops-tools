f7f4e3fc528ac6adb7f2f285ebd88926fd73210c
Othman Madjoudj <omadjoudj@mirantis.com>
2023-10-12 14:58:49 +0200

    Add collecting logs matching certain substring

--- mosk-log-collect-snippets/get-k8s-pod-ctn-logs.sh
+++ mosk-log-collect-snippets/get-k8s-pod-ctn-logs.sh
@@ -2,13 +2,28 @@
 #~omadjoudj
 # Get K8s pod/container logs from a given namespace
 
-namespace=$1
 
-echo "Collecting logs from the namespace $namespace:"
+if [ $# -eq 2 ]; then
+    namespace=$1
+    pod_substring=$2
 
-kubectl get pods --namespace $namespace -o jsonpath='{range .items[*]}{.metadata.name}{"\n"}{end}' | \
+    echo "Collecting logs from the namespace $namespace and filtering only pod containing $pod_substring"
+    kubectl get pods --namespace $namespace -o jsonpath='{range .items[*]}{.metadata.name}{"\n"}{end}' | grep $pod_substring | \
     while read -r pod; do
         kubectl logs "$pod" --namespace $namespace --all-containers --prefix --timestamps > ${namespace}__${pod}.log
         echo -n '.'
     done
+elif [ $# -eq 1 ]; then
+    namespace=$1
+    echo "Collecting logs from the namespace $namespace:"
+    kubectl get pods --namespace $namespace -o jsonpath='{range .items[*]}{.metadata.name}{"\n"}{end}'  | \
+    while read -r pod; do
+        kubectl logs "$pod" --namespace $namespace --all-containers --prefix --timestamps > ${namespace}__${pod}.log
+        echo -n '.'
+    done
+else
+    echo "Usage: $0 <namespace> [sub string in pod name]>"
+    exit 1
+fi
+
 echo
