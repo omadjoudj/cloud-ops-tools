4207cf0e0f6249a8976a1ddfa7d5d8c9c2c761d0
Othman Madjoudj <omadjoudj@mirantis.com>
2023-10-13 12:05:15 +0200

    Reverse last commit

--- ops.alias.sh
+++ ops.alias.sh
@@ -3,10 +3,10 @@ alias k='kubectl'
 . ~/.kubectl_bash_compl
 complete -o default -F __start_kubectl k
 
-alias mcc="export KUBECONFIG=./mcc.kubeconfig;"
-alias mosk="export KUBECONFIG=./mosk.kubeconfig;"
+alias mcc="export KUBECONFIG=$PWD/mcc.kubeconfig;"
+alias mosk="export KUBECONFIG=$PWD/mosk.kubeconfig;"
 alias rk="kubectl exec -n rook-ceph deploy/rook-ceph-tools -it --"
-alias os="kubectl exec -it -n openstack deploy/keystone-client -it --"
+alias ks="kubectl exec -it -n openstack deploy/keystone-client -it --"
 alias silences="kubectl -n stacklight exec sts/prometheus-alertmanager -c prometheus-alertmanager -- amtool --alertmanager.url http://127.0.0.1:9093 silence"
 alias silence-add="kubectl -n stacklight exec sts/prometheus-alertmanager -c prometheus-alertmanager -- amtool --alertmanager.url http://127.0.0.1:9093 silence add -a '<YOUR_NAME>' -d '2h'"
 alias silence-all='silence-add -c "Comment" alertname=~".+"'
