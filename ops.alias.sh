9a04569454252e1b15a854f5d9746ecdd0a0c28b
Othman Madjoudj <omadjoudj@mirantis.com>
2023-10-13 15:21:38 +0200

    Update the aliases

--- ops.alias.sh
+++ ops.alias.sh
@@ -7,6 +7,6 @@ alias mcc="export KUBECONFIG=$PWD/mcc.kubeconfig;"
 alias mosk="export KUBECONFIG=$PWD/mosk.kubeconfig;"
 alias rk="kubectl exec -n rook-ceph deploy/rook-ceph-tools -it --"
 alias ks="kubectl exec -it -n openstack deploy/keystone-client -it --"
-alias silences="kubectl -n stacklight exec sts/prometheus-alertmanager -c prometheus-alertmanager -- amtool --alertmanager.url http://127.0.0.1:9093 silence"
+alias silence-list="kubectl -n stacklight exec sts/prometheus-alertmanager -c prometheus-alertmanager -- amtool --alertmanager.url http://127.0.0.1:9093 silence"
 alias silence-add="kubectl -n stacklight exec sts/prometheus-alertmanager -c prometheus-alertmanager -- amtool --alertmanager.url http://127.0.0.1:9093 silence add -a '<YOUR_NAME>' -d '2h'"
 alias silence-all='silence-add -c "Comment" alertname=~".+"'
