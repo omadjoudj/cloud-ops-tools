76fb53c9ab2a7e3883d9cfae8c97dbb64e5a8613
Othman Madjoudj <omadjoudj@mirantis.com>
2023-10-27 11:05:12 +0200

    Update aliases

--- ops.alias.sh
+++ ops.alias.sh
@@ -8,7 +8,8 @@ alias mosk="export KUBECONFIG=$PWD/mosk.kubeconfig;"
 alias rk="kubectl exec -n rook-ceph deploy/rook-ceph-tools -it --"
 alias ks="kubectl exec -it -n openstack deploy/keystone-client -it --"
 alias silence-list="kubectl -n stacklight exec sts/prometheus-alertmanager -c prometheus-alertmanager -- amtool --alertmanager.url http://127.0.0.1:9093 silence"
-alias silence-add="kubectl -n stacklight exec sts/prometheus-alertmanager -c prometheus-alertmanager -- amtool --alertmanager.url http://127.0.0.1:9093 silence add -a '<YOUR_NAME>' -d '2h'"
+alias silence-add="kubectl -n stacklight exec sts/prometheus-alertmanager -c prometheus-alertmanager -- amtool --alertmanager.url http://127.0.0.1:9093 silence add -a $USER -d '2h'"
 alias silence-all='silence-add -c "Comment" alertname=~".+"'
+alias silence-upgrade='silence-add -c "alertname!=TestVMCreateError|TestVMPingError|TestVMCountTooHigh|TestVMCreateTooLong"'
 
 export KUBE_EDITOR='code --wait'
