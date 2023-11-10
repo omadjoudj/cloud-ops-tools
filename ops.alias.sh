590afd8e2431033f4140e46a9913a89e340110fd
Othman Madjoudj <omadjoudj@mirantis.com>
2023-11-10 09:46:44 +0100

    Adjust alert silencing

--- ops.alias.sh
+++ ops.alias.sh
@@ -10,6 +10,6 @@ alias ks="kubectl exec -it -n openstack deploy/keystone-client -it --"
 alias silence-list="kubectl -n stacklight exec sts/prometheus-alertmanager -c prometheus-alertmanager -- amtool --alertmanager.url http://127.0.0.1:9093 silence"
 alias silence-add="kubectl -n stacklight exec sts/prometheus-alertmanager -c prometheus-alertmanager -- amtool --alertmanager.url http://127.0.0.1:9093 silence add -a $USER "
 alias silence-all='silence-add -c "Comment" alertname=~".+"'
-alias silence-upgrade='silence-add -d 7d -c "MOSK Upgrade"  "alertname!=TestVMCreateError|TestVMPingError|TestVMCountTooHigh|TestVMCreateTooLong|OpenstackServiceInternalApiOutage|OpenstackServicePublicApiOutage|OpenstackPublicAPI5xxCritical|OpenstackAPI5xxCritical|OpenstackAPI401Critical"'
+alias silence-upgrade='silence-add -d 7d -c "MOSK Upgrade"  "alertname!=Watchdog|TestVMCreateError|TestVMPingError|TestVMCountTooHigh|TestVMCreateTooLong|OpenstackServiceInternalApiOutage|OpenstackServicePublicApiOutage|OpenstackPublicAPI5xxCritical|OpenstackAPI5xxCritical|OpenstackAPI401Critical"'
 
 export KUBE_EDITOR='code --wait'
