3fc0d1dc7a391108a28c8878f917655a66d64317
Othman Madjoudj <omadjoudj@mirantis.com>
2023-11-20 12:05:37 +0100

    Adjust the k8s log collector output

--- get-k8s-logs.sh
+++ get-k8s-logs.sh
@@ -7,7 +7,7 @@ if [ $# -eq 2 ]; then
     namespace=$1
     pod_substring=$2
 
-    echo "Collecting logs from the namespace $namespace and filtering only pod containing $pod_substring"
+    echo "Collecting logs from the namespace << $namespace >> and filtering only pod containing the string << $pod_substring >>"
     kubectl get pods --namespace $namespace -o jsonpath='{range .items[*]}{.metadata.name}{"\n"}{end}' | grep $pod_substring | \
     while read -r pod; do
         kubectl logs "$pod" --namespace $namespace --all-containers --prefix --timestamps > ${namespace}__${pod}.log
@@ -15,7 +15,7 @@ if [ $# -eq 2 ]; then
     done
 elif [ $# -eq 1 ]; then
     namespace=$1
-    echo "Collecting logs from the namespace $namespace:"
+    echo "Collecting logs from the namespace << $namespace >>"
     kubectl get pods --namespace $namespace -o jsonpath='{range .items[*]}{.metadata.name}{"\n"}{end}'  | \
     while read -r pod; do
         kubectl logs "$pod" --namespace $namespace --all-containers --prefix --timestamps > ${namespace}__${pod}.log
