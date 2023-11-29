76509f602676ada940f9c693061b2fd7f1af840c
Othman Madjoudj <omadjoudj@mirantis.com>
2023-11-29 13:46:56 +0100

    Sync aliases

--- ops.alias.sh
+++ ops.alias.sh
@@ -9,5 +9,10 @@ alias silence-list="kubectl -n stacklight exec sts/prometheus-alertmanager -c pr
 alias silence-add="kubectl -n stacklight exec sts/prometheus-alertmanager -c prometheus-alertmanager -- amtool --alertmanager.url http://127.0.0.1:9093 silence add -a $USER "
 alias silence-all='silence-add -d 2h -c "Temporary silence for 2H"  "alertname!~Watchdog|TestVMCreateError|TestVMPingError|TestVMCountTooHigh|TestVMCreateTooLong|OpenstackServiceInternalApiOutage|OpenstackServicePublicApiOutage|OpenstackPublicAPI5xxCritical|OpenstackAPI5xxCritical|OpenstackAPI401Critical"'
 alias silence-upgrade='silence-add -d 7d -c "MOSK Upgrade"  "alertname!~Watchdog|TestVMCreateError|TestVMPingError|TestVMCountTooHigh|TestVMCreateTooLong|OpenstackServiceInternalApiOutage|OpenstackServicePublicApiOutage|OpenstackPublicAPI5xxCritical|OpenstackAPI5xxCritical|OpenstackAPI401Critical"'
-
 export KUBE_EDITOR='code --wait'
+
+
+function k_get_pods_per_node 
+{ 
+    kubectl get pods -A -o wide  --field-selector spec.nodeName=$1 
+}
