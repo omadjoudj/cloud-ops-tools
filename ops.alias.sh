a055e6e2159d302200c8ce1b497bd54fec7b429b
Othman Madjoudj <omadjoudj@mirantis.com>
2024-04-11 14:42:08 +0200

    Comment out broken alises

--- ops.alias.sh
+++ ops.alias.sh
@@ -8,8 +8,8 @@ alias o="kubectl exec -it -n openstack deploy/keystone-client -c keystone-client
 alias silence-list="kubectl -n stacklight exec sts/prometheus-alertmanager -c prometheus-alertmanager -- amtool --alertmanager.url http://127.0.0.1:9093 silence"
 alias silence-del="kubectl -n stacklight exec sts/prometheus-alertmanager -c prometheus-alertmanager -- amtool --alertmanager.url http://127.0.0.1:9093 silence expire"
 alias silence-add="kubectl -n stacklight exec sts/prometheus-alertmanager -c prometheus-alertmanager -- amtool --alertmanager.url http://127.0.0.1:9093 silence add -a $USER "
-alias silence-all='silence-add -d 2h -c "Temporary silence for 2H"  "alertname!~Watchdog|TestVMCreateError|TestVMPingError|TestVMCountTooHigh|TestVMCreateTooLong|OpenstackServiceInternalApiOutage|OpenstackServicePublicApiOutage|OpenstackPublicAPI5xxCritical|OpenstackAPI5xxCritical|OpenstackAPI401Critical"'
-alias silence-upgrade='silence-add -d 7d -c "MOSK Upgrade"  "alertname!~Watchdog|TestVMCreateError|TestVMPingError|TestVMCountTooHigh|TestVMCreateTooLong|OpenstackServiceInternalApiOutage|OpenstackServicePublicApiOutage|OpenstackPublicAPI5xxCritical|OpenstackAPI5xxCritical|OpenstackAPI401Critical"'
+#alias silence-all='silence-add -d 2h -c "Temporary silence for 2H"  "alertname!~Watchdog|TestVMCreateError|TestVMPingError|TestVMCountTooHigh|TestVMCreateTooLong|OpenstackServiceInternalApiOutage|OpenstackServicePublicApiOutage|OpenstackPublicAPI5xxCritical|OpenstackAPI5xxCritical|OpenstackAPI401Critical"'
+#alias silence-upgrade='silence-add -d 7d -c "MOSK Upgrade"  "alertname!~Watchdog|TestVMCreateError|TestVMPingError|TestVMCountTooHigh|TestVMCreateTooLong|OpenstackServiceInternalApiOutage|OpenstackServicePublicApiOutage|OpenstackPublicAPI5xxCritical|OpenstackAPI5xxCritical|OpenstackAPI401Critical"'
 export KUBE_EDITOR='code --wait'
 
 alias benv='. ~/kenv `cat ~/workspace/mos_access/b.com_envs  | fzf`'
