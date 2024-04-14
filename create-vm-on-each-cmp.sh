8f16beb27f05008c78f5cd5c6032c9b57e6d40a5
Othman Madjoudj <omadjoudj@mirantis.com>
2024-04-15 01:27:32 +0200

    Update create-vm-on-each-cmp.sh

--- create-vm-on-each-cmp.sh
+++ create-vm-on-each-cmp.sh
@@ -1,13 +1,21 @@
 #!/bin/bash
 # Contact: ~omadjoudj
 
-IMG="Cirros-6.0"
+PREFIX="omadjoudj-opscare-upgrade-canary-workload"
+IMG="healthcheck-vm"
 SSHKEY="omadjoudj-opscare-pubkey"
-NETID=""
-FLAVOR="m1.extra_tiny_test"
+NETID="healthcheck-net"
+PUBLIC_NET="public-prod"
+FLAVOR="m1.healthcheck"
+SEC_GROUP="allow_all"
 
-
-for cmp in `kubectl exec -it -n openstack deploy/keystone-client -it -- openstack compute service list -f csv --quote minimal -c Binary -c Host -c State | grep nova-compute`;
+for cmp in `openstack compute service list -f csv --quote minimal -c Binary -c Host -c State | grep nova-compute | cut -d, -f2`;
 do
-    echo openstack server create --flavor $FLAVOR --image $IMG --key-name $SSHKEY --network $NETID --availability-zone nova:<compute-node> opscare-workload-mon-${cmp}
+    #openstack server create --wait --flavor $FLAVOR --image $IMG --key-name $SSHKEY --network $NETID --availability-zone nova:<compute-node> ${PREFIX}-workload-mon-${cmp}
+    #
+    vm_id=`openstack server create --flavor $FLAVOR --image $IMG --network $NETID --availability-zone nova:$cmp -f value -c id ${PREFIX}-workload-mon-${cmp}`
+    fip=`openstack floating ip create $PUBLIC_NET -f value -c floating_ip_address`
+    openstack server add floating ip $vm_id $fip
+    openstack server add security group $vm_id $SEC_GROUP
+    openstack --os-compute-api-version 2.26 server set --tag openstack.lcm.mirantis.com:prober $vm_id
 done
