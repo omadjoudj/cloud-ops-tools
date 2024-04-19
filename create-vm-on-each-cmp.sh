196284c0d125881126c9411625c62837c136044e
Othman Madjoudj <omadjoudj@mirantis.com>
2024-04-19 17:31:36 +0200

    Fix security group creation

--- create-vm-on-each-cmp.sh
+++ create-vm-on-each-cmp.sh
@@ -1,19 +1,25 @@
 #!/bin/bash
 # Contact: ~omadjoudj
 
-PREFIX="omadjoudj-opscare-upgrade-canary-workload"
+PREFIX="omadjoudj-opscare-upgrade-canary-workload-mon"
 IMG="healthcheck-vm"
 SSHKEY="omadjoudj-opscare-pubkey"
 NETID="healthcheck-net"
 PUBLIC_NET="public-prod"
 FLAVOR="m1.healthcheck"
-SEC_GROUP="allow_all"
+SEC_GROUP="omadjoudj_opscare_allow_icmp"
+
+# Create sec group and its rule
+openstack security group create $SEC_GROUP --description "Allows ICMP traffic for Cloudprober"
+openstack security group rule create $SEC_GROUP --protocol icmp --remote-ip 0.0.0.0/0 --ingress
+openstack security group rule create $SEC_GROUP --protocol icmp --remote-ip 0.0.0.0/0 --egress
+##
 
 for cmp in `openstack compute service list -f csv --quote minimal -c Binary -c Host -c State | grep nova-compute | cut -d, -f2`;
 do
-    #openstack server create --wait --flavor $FLAVOR --image $IMG --key-name $SSHKEY --network $NETID --availability-zone nova:<compute-node> ${PREFIX}-workload-mon-${cmp}
+    #openstack server create --wait --flavor $FLAVOR --image $IMG --key-name $SSHKEY --network $NETID --availability-zone nova:<compute-node> ${PREFIX}-${cmp}
     #
-    vm_id=`openstack server create --flavor $FLAVOR --image $IMG --network $NETID --availability-zone nova:$cmp -f value -c id ${PREFIX}-workload-mon-${cmp}`
+    vm_id=`openstack server create --flavor $FLAVOR --image $IMG --network $NETID --availability-zone nova:$cmp -f value -c id ${PREFIX}-${cmp}`
     fip=`openstack floating ip create $PUBLIC_NET -f value -c floating_ip_address`
     openstack server add floating ip $vm_id $fip
     openstack server add security group $vm_id $SEC_GROUP
