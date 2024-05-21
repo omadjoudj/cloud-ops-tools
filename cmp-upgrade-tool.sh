cf8031247745f81c8669a0c89a65214589d3f545
Othman Madjoudj <omadjoudj@mirantis.com>
2024-05-21 20:15:57 +0200

    - Reduce openstack calls in check_cmp_upgrade_readiness function - Refactor: reduce repitition with node_safe_release_lock - Generate inventory from nodes with openstack-compute-node=enabled label to reduce false positives - Minor cosmetic changes

--- cmp-upgrade-tool.sh
+++ cmp-upgrade-tool.sh
@@ -2,6 +2,8 @@
 # Contact: omadjoudj
 # TODO: Integrate it with Migrator tool and create a function around it to drain the node or stop the workload before releasing the lock
 
+set -euo pipefail
+
 
 KEYSTONE_POD_PREFIX="kubectl exec -it -n openstack deploy/keystone-client -c keystone-client -it --"
 TOOL_NAME="custom-opscare-openstack-cmp-upgrade-tool"
@@ -11,25 +13,34 @@ CMP_INVENTORY="/tmp/cmp_inventory_$(date +%Y%m%d%H%M%S)_$$_$RANDOM.txt"
 function check_cmp_upgrade_readiness()
 {
     local cmp=$1
-    
-    local vm_in_shutoff_state="$( $KEYSTONE_POD_PREFIX openstack server list --all -n -f value --limit -1 --status SHUTOFF --host $cmp | grep -c 'SHUTOFF')"
-    local all_vms="$( $KEYSTONE_POD_PREFIX openstack server list --all -n -f value --limit -1 --host $cmp | grep -c '')"
-
-    # kubectl puts an extra line 
-    if [[ "$vm_in_shutoff_state" == "$all_vms" ]] ; then
-        return 0
-    else
+    local non_running_vms="$( $KEYSTONE_POD_PREFIX openstack server list --all -n -f value --limit -1 --host $cmp |grep -v -w SHUTOFF )"
+    if [[ -n "$non_running_vms" ]]; then
+        echo "ERROR: $cmp still has running VMs."
+        echo "$non_running_vms" | awk '{print $1}'
         return 1
     fi
+    return 0
+}
 
+function node_safe_release_lock()
+{
+    local cmp=$1
+    check_cmp_upgrade_readiness $cmp
+    result=$?
+    if [ $result -eq 0 ]; then
+        remove_nodeworkloadlock $cmp
+    else 
+        echo "ERROR: Node $cmp failed the readiness checks. The node must be Empty or have its workload in SHUTOFF state"
+        exit 2  
+    fi 
 }
 
 function refresh_cmp_inventory()
 {
     
     #$KEYSTONE_POD_PREFIX openstack compute service list --service nova-compute -f value -c Host > $CMP_INVENTORY
-    echo "Refreshing compute node inventory"
-    kubectl get nodes -o json 2>/dev/null | jq -j '.items[] | .metadata.name, " ", .metadata.labels."kaas.mirantis.com/machine-name", "\n"' | sort -k 2 | grep 'cmp' > $CMP_INVENTORY
+    echo "INFO: Refreshing compute node inventory"
+    kubectl get nodes -l openstack-compute-node=enabled -o json | jq -j '.items[] | .metadata.name, " ", .metadata.labels."kaas.mirantis.com/machine-name", "\n"' | sort -k 2 | grep 'cmp' > $CMP_INVENTORY
 }
 
 function create_nodeworkloadlock()
@@ -74,12 +85,22 @@ if [ $# -eq 0 ]; then
    exit 1
 fi 
 
-refresh_cmp_inventory
-
 case "$1" in
     lock-all-nodes)
-        echo "Creating a custom NodeWorkloadLock on all compute nodes"
+        refresh_cmp_inventory
+        echo "INFO: Creating a custom NodeWorkloadLock on all compute nodes"
         lock_all_nodes
+        ;;
+    
+    node-release-lock)
+        if [ -z "$2" ]; then
+            echo "ERROR: No Node specified."
+            usage
+            exit 1
+        else
+            refresh_cmp_inventory
+            node_safe_release_lock $2
+        fi
         ;;
     rack-release-lock)
         if [ -z "$2" ]; then
@@ -87,37 +108,17 @@ case "$1" in
             usage
             exit 1
         else
+            refresh_cmp_inventory
             if grep -q "$2" $CMP_INVENTORY ; then
-                echo "Starting to Release NodeWorkloadLock on the nodes of the rack $2"
+                echo "INFO: Starting to Release NodeWorkloadLock on the nodes of the rack $2:"
+                grep "$2" $CMP_INVENTORY
                 for i in `grep "$2" $CMP_INVENTORY | awk '{print $1}'`; 
                 do
-                    check_cmp_upgrade_readiness $i
-                    result=$?
-                    if [ $result -eq 0 ]; then
-                        remove_nodeworkloadlock $i
-                    else 
-                        echo "ERROR: Node $i failed the readiness checks. The node must be Empty or have its workload in SHUTOFF state"
-                        exit 2  
-                    fi 
+                    node_safe_release_lock $i
                 done
             else
                 echo "ERROR: Rack $2 not found in the inventory"
-            fi
-        fi
-        ;;
-    node-release-lock)
-        if [ -z "$2" ]; then
-            echo "ERROR: No Node specified."
-            usage
-            exit 1
-        else
-            check_cmp_upgrade_readiness $2
-            result=$?
-            if [ $result -eq 0 ]; then
-                remove_nodeworkloadlock $2
-            else 
-                echo "ERROR: Node $2 failed the readiness checks. The node must be Empty or have its workload in SHUTOFF state"
-                exit 2  
+                exit 1
             fi
         fi
         ;;
