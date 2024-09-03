76d82e0de58c6448893238f77c787651e5cc4301
Othman Madjoudj <omadjoudj@mirantis.com>
2024-09-03 17:08:23 +0200

    Comment out KUBE_EDITOR var

--- ops.alias.sh
+++ ops.alias.sh
@@ -10,7 +10,7 @@ alias silence-del="kubectl -n stacklight exec sts/prometheus-alertmanager -c pro
 alias silence-add="kubectl -n stacklight exec sts/prometheus-alertmanager -c prometheus-alertmanager -- amtool --alertmanager.url http://127.0.0.1:9093 silence add -a $USER "
 #alias silence-all='silence-add -d 2h -c "Temporary silence for 2H"  "alertname!~Watchdog|TestVMCreateError|TestVMPingError|TestVMCountTooHigh|TestVMCreateTooLong|OpenstackServiceInternalApiOutage|OpenstackServicePublicApiOutage|OpenstackPublicAPI5xxCritical|OpenstackAPI5xxCritical|OpenstackAPI401Critical"'
 #alias silence-upgrade='silence-add -d 7d -c "MOSK Upgrade"  "alertname!~Watchdog|TestVMCreateError|TestVMPingError|TestVMCountTooHigh|TestVMCreateTooLong|OpenstackServiceInternalApiOutage|OpenstackServicePublicApiOutage|OpenstackPublicAPI5xxCritical|OpenstackAPI5xxCritical|OpenstackAPI401Critical"'
-export KUBE_EDITOR='code --wait'
+#export KUBE_EDITOR='code --wait'
 
 alias benv='. ~/kenv `cat ~/workspace/mos_access/b.com_envs  | fzf`'
 
