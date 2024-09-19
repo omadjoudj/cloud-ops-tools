87ac5d5ef22519a35a60a4c1fa145fd7f6c769c7
Othman Madjoudj <omadjoudj@mirantis.com>
2024-09-19 15:22:04 +0200

    Use k context

--- ops.alias.sh
+++ ops.alias.sh
@@ -4,15 +4,11 @@ alias m='multipass'
 source <(kubectl completion zsh)
 complete -o default -F __start_kubectl k
 
-alias r="kubectl exec -n rook-ceph deploy/rook-ceph-tools -it --"
-alias o="kubectl exec -it -n openstack deploy/keystone-client -c keystone-client -it --"
+#alias r="kubectl exec -n rook-ceph deploy/rook-ceph-tools -it --"
+#alias o="kubectl exec -it -n openstack deploy/keystone-client -c keystone-client -it --"
 alias silence-list="kubectl -n stacklight exec sts/prometheus-alertmanager -c prometheus-alertmanager -- amtool --alertmanager.url http://127.0.0.1:9093 silence"
 alias silence-del="kubectl -n stacklight exec sts/prometheus-alertmanager -c prometheus-alertmanager -- amtool --alertmanager.url http://127.0.0.1:9093 silence expire"
 alias silence-add="kubectl -n stacklight exec sts/prometheus-alertmanager -c prometheus-alertmanager -- amtool --alertmanager.url http://127.0.0.1:9093 silence add -a $USER "
-#alias silence-all='silence-add -d 2h -c "Temporary silence for 2H"  "alertname!~Watchdog|TestVMCreateError|TestVMPingError|TestVMCountTooHigh|TestVMCreateTooLong|OpenstackServiceInternalApiOutage|OpenstackServicePublicApiOutage|OpenstackPublicAPI5xxCritical|OpenstackAPI5xxCritical|OpenstackAPI401Critical"'
-#alias silence-upgrade='silence-add -d 7d -c "MOSK Upgrade"  "alertname!~Watchdog|TestVMCreateError|TestVMPingError|TestVMCountTooHigh|TestVMCreateTooLong|OpenstackServiceInternalApiOutage|OpenstackServicePublicApiOutage|OpenstackPublicAPI5xxCritical|OpenstackAPI5xxCritical|OpenstackAPI401Critical"'
-#export KUBE_EDITOR='code --wait'
 
-alias benv='. ~/kenv `cat ~/workspace/mos_access/b.com_envs  | fzf`'
-
+alias benv='. ~/kenv `cat ~/.kube/config| grep "\- name:" | cut -d: -f2 | tr -d " " | fzf`'
 
