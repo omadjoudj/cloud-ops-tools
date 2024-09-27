28f09be8c6fc1a27e8daa20bab036e07a97debd4
Othman Madjoudj <omadjoudj@mirantis.com>
2024-09-27 21:56:48 +0200

    Add new Rack reboot script

---
+++ cmp-reboot-rack.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+#
+# A: ~omadjoudj
+#
+
+set -euo pipefail
+
+cluster_ns="$(kubectl  --context $KUBE_CONTEXT get cluster -A --no-headers | grep -v 'default' | awk '{print $1}')"
+
+echo "About to reboot the compute nodes on the rack $1 on $KUBE_CONTEXT"
+read -p "Proceed? (y/n): " -n 1 -r
+echo
+if [[ ! $REPLY =~ ^[Yy]$ ]]; then
+    echo "Command cancelled"
+    return 1
+else
+	kubectl  get bmh -n $cluster_ns -o wide  --context $KUBE_CONTEXT | grep cmp | grep $1 | awk '{print $1}' | xargs -P20 -I% kubectl  --context $KUBE_CONTEXT -n $cluster_ns  patch bmh % --type=merge -p "{\"spec\":{\"online\":false}}"
+
+	sleep 120
+
+	kubectl  get bmh -n $cluster_ns -o wide  --context $KUBE_CONTEXT | grep cmp | grep $1 | awk '{print $1}' | xargs -P20 -I% kubectl  --context $KUBE_CONTEXT -n $cluster_ns  patch bmh % --type=merge -p "{\"spec\":{\"online\":true}}"
+fi
--- ops.alias.sh
+++ ops.alias.sh
@@ -6,9 +6,9 @@ complete -o default -F __start_kubectl k
 
 #alias r="kubectl exec -n rook-ceph deploy/rook-ceph-tools -it --"
 #alias o="kubectl exec -it -n openstack deploy/keystone-client -c keystone-client -it --"
-alias silence-list="kubectl -n stacklight exec sts/prometheus-alertmanager -c prometheus-alertmanager -- amtool --alertmanager.url http://127.0.0.1:9093 silence"
-alias silence-del="kubectl -n stacklight exec sts/prometheus-alertmanager -c prometheus-alertmanager -- amtool --alertmanager.url http://127.0.0.1:9093 silence expire"
-alias silence-add="kubectl -n stacklight exec sts/prometheus-alertmanager -c prometheus-alertmanager -- amtool --alertmanager.url http://127.0.0.1:9093 silence add -a $USER "
+alias silence-list="kubectl --context $KUBE_CONTEXT -n stacklight exec sts/prometheus-alertmanager -c prometheus-alertmanager -- amtool --alertmanager.url http://127.0.0.1:9093 silence"
+alias silence-del="kubectl --context $KUBE_CONTEXT -n stacklight exec sts/prometheus-alertmanager -c prometheus-alertmanager -- amtool --alertmanager.url http://127.0.0.1:9093 silence expire"
+alias silence-add="kubectl --context $KUBE_CONTEXT  -n stacklight exec sts/prometheus-alertmanager -c prometheus-alertmanager -- amtool --alertmanager.url http://127.0.0.1:9093 silence add -a $USER "
 
 alias benv='export KUBECONFIG=~/workspace/envs-access/B.com/booking_kubeconfig ; . ~/kenv `kubectl config get-contexts --no-headers=false -o name | fzf`'
 
