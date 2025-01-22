374fddfe44b1ceefc6fd9cde60a09d588eb18940
Othman Madjoudj <omadjoudj@mirantis.com>
2025-01-22 15:27:24 +0000

    Add disable subnuma clustering playbook

--- ops.alias.sh
+++ ops.alias.sh
@@ -4,4 +4,4 @@ alias m='multipass'
 source <(kubectl completion zsh)
 
 alias benv=' . ~/kenv `kubectl config get-contexts --no-headers=false -o name | fzf`'
-
+alias gerrit-push='git push origin HEAD:refs/for/`git branch --show-current`'
