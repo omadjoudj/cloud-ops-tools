4a23ca0460638244174df93d8d97dd5a4ededc23
Othman Madjoudj <omadjoudj@mirantis.com>
2025-07-18 15:11:34 +0000

    Add normalizer alias

--- ops.alias.sh
+++ ops.alias.sh
@@ -5,3 +5,4 @@ source <(kubectl completion zsh)
 
 alias benv=' . ~/kenv `kubectl config get-contexts --no-headers=false -o name | fzf`'
 alias gerrit-push='git push origin HEAD:refs/for/`git branch --show-current`'
+alias normalizer='docker run -it -v `pwd`:/tmp/repo docker-dev-kaas-virtual.mcp.mirantis.com/services/diff-exporter:0.0.1.dev92-alpine3.20.6-20250416174021 diff-normalize --path /tmp/repo'
