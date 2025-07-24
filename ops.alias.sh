8c9e60cb3923143f852b8a757875e8fa2928e14a
Othman Madjoudj <omadjoudj@mirantis.com>
2025-07-24 14:25:26 +0000

    Adjust alias name

--- ops.alias.sh
+++ ops.alias.sh
@@ -5,4 +5,4 @@ source <(kubectl completion zsh)
 
 alias benv='kubectx'
 alias gerrit-push='git push origin HEAD:refs/for/`git branch --show-current`'
-alias normalizer='docker run -it -v `pwd`:/tmp/repo docker-dev-kaas-virtual.mcp.mirantis.com/services/diff-exporter:0.0.1.dev92-alpine3.20.6-20250416174021 diff-normalize --path /tmp/repo'
+alias diff-normalizer='docker run -it -v `pwd`:/tmp/repo docker-dev-kaas-virtual.mcp.mirantis.com/services/diff-exporter:0.0.1.dev92-alpine3.20.6-20250416174021 diff-normalize --path /tmp/repo'
