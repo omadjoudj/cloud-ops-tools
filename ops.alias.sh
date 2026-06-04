55fd8f1389de90d923cf573a8d5eb4be0bdec3bd
Othman Madjoudj <omadjoudj@mirantis.com>
2026-06-04 05:36:18 -0400

    benv -> kenv

--- ops.alias.sh
+++ ops.alias.sh
@@ -3,7 +3,7 @@ alias m='multipass'
 #source <(kubectl completion bash)
 source <(kubectl completion zsh)
 
-alias benv='kubectx'
+alias kenv='kubectx'
 alias gerrit-push='git push origin HEAD:refs/for/`git branch --show-current`'
 alias diff-normalizer='docker run -it -v `pwd`:/tmp/repo docker-dev-kaas-virtual.mcp.mirantis.com/services/diff-exporter:0.0.1.dev92-alpine3.20.6-20250416174021 diff-normalize --path /tmp/repo'
 
